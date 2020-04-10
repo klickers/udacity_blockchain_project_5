4b6add71c38cbfad1bedb15ee347ad2a4d79d371
klickers <bluebirid228@gmail.com>
2020-04-10 10:11:31 -0700

    updated

--- app/src/index.js
+++ app/src/index.js
@@ -53,7 +53,7 @@ const App = {
 window.App = App;
 
 window.addEventListener("load", async function() {
-  /*if (window.ethereum) {
+  if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
@@ -61,9 +61,9 @@ window.addEventListener("load", async function() {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
-} */
+}
 
-  App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+  //App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
 
   App.start();
 });
